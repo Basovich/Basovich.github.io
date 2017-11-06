c7ab7e26bf81b692952d21e96466eeafe5eb3f10
Vadim <yakovenko86@ukr.net>
2017-11-06 10:13:35 +0200

    test-work

---
+++ test_project/js/like.js
@@ -0,0 +1,8 @@
+$(function(){
+	$(".card__like-up--hover").click(function() {
+	  return $(".card__like-up--cheked").css( "display", "block");
+	});
+	$(".card__like-down--hover").click(function() {
+	  return $(".card__like-down--cheked").css( "display", "block");
+	});
+});
