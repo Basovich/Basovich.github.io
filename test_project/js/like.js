3d444dab26776eb3354f98670a695c68b795d398
Vadim <yakovenko86@ukr.net>
2017-11-07 00:44:54 +0200

    Добавил функционал по лайкамю. кликаються отдельно, лайкнуть можно только 1 раз.

--- test_project/js/like.js
+++ test_project/js/like.js
@@ -1,8 +1,12 @@
-$(function(){
-	$(".card__like-up--hover").click(function() {
-	  return $(".card__like-up--cheked").css( "display", "block");
-	});
+$(function(){	
 	$(".card__like-down--hover").click(function() {
-	  return $(".card__like-down--cheked").css( "display", "block");
+		$(this).css( "pointer-events", "none");
+		$(this).prev(".card__like-down--cheked").css( "display", "block");
+		$(this).next(".card__like-up--hover").css( "pointer-events", "none");	  
+	});
+	$(".card__like-up--hover").click(function() {
+		$(this).css( "pointer-events", "none");
+		$(this).prev(".card__like-down--hover").css( "pointer-events", "none");
+	  $(this).next(".card__like-up--cheked").css( "display", "block");
 	});
 });
