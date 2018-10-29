73c9fdf524a20ddf94eb6f19ea7971a6d8940c25
Vadim <yakovenko86@ukr.net>
2018-10-29 23:41:55 +0200

    del

--- it/js/index.js
+++ it/js/index.js
@@ -4,13 +4,17 @@
     $('.slider-preview').slick({
         slidesToShow: 6,
         slidesToScroll: 1,
-        // vertical:true,
+        vertical:true,
         arrows:true,
         speed: 500,
         infinite: false,
         pauseOnHover:true,
         pauseOnDotsHover:true,
-        swipeToSlide: true
+        draggable: false,
+        swipe: false,
+        useTransform: true,
+        prevArrow: '<div class="prev-slide"></div>',
+        nextArrow: '<div class="next-slide"></div>'
     });
 
     $('.big-slider').slick({
@@ -21,11 +25,56 @@
         infinite: false,
         pauseOnHover:true,
         pauseOnDotsHover:true,
-        swipeToSlide: true
+        draggable: false,
+        swipe: false,
+        useTransform: true,
+        fade: true,
+        prevArrow: '<div class="prev-slide"></div>',
+        nextArrow: '<div class="next-slide"></div>'
     });
 
 
-    $('.js-select').on('click', function() {
-        $(this).find('.list').addClass('show-list');
+    // Open and close select
+    $('.nice-select').on('click', function(e) {
+        $('.nice-select').not($(this)).removeClass('open');
+        $(this).toggleClass('open');
     });
+    // close select if click target it`s not select
+    $(document).on('mouseup', function (e){
+        var select = $(".nice-select"),
+            calendar = $('.data-input-fields');
+
+        if (!select.is(e.target)
+            && select.has(e.target).length === 0) {
+            select.removeClass('open');
+        }
+
+        if (!calendar.is(e.target)
+            && calendar.has(e.target).length === 0) {
+            calendar.removeClass('isOpened');
+        }
+    });
+    //записываем выбор в селект
+    $('.option').on('click', function() {
+        var value = $(this).text();
+        $(this).parents('.nice-select').find('span.current').text(value);
+        $(this).parents('.nice-select').find('.option').removeClass('selected').removeClass('focus');
+        $(this).addClass('selected').addClass('focus');
+    });
+
+
+    //Slick
+    $('.slider-preview .slide.slick-slide').on('click', function() {
+
+        // console.log($(this));
+        // console.log($(this).attr('data-slick-index'));
+        $('.big-slider').slick('slickGoTo', ($(this).attr('data-slick-index')) );
+    });
+
+
+    $('.data-input-fields').on('click', function() {
+        $(this).toggleClass('isOpened');
+    });
+
+
 })(jQuery);
